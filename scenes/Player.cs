b6d5124d2c6827da6ffe3271251ea6b54197ec6d
Guilherme Konzen <108848546+guikonzen1@users.noreply.github.com>
2025-05-19 13:14:14 -0400

    Update Player.cs

--- scenes/Player.cs
+++ scenes/Player.cs
@@ -4,6 +4,8 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+	private float gravity = 20f;
+	private float jumpForce = 10f;
 	public float sensitvity = 0.01f;
 	[Export] public float Speed = 5.5f;
 	public Camera3D camera;
@@ -14,6 +16,7 @@ public partial class Player : CharacterBody3D
 
 	public override void _UnhandledInput(InputEvent evt)
 	{
+		//Movimento do mouse
 		if (evt is InputEventMouseMotion mouseMotion)
 		{
 			RotateY(-mouseMotion.Relative.X * sensitvity);
@@ -27,16 +30,27 @@ public partial class Player : CharacterBody3D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		var velo = Velocity;
-		var inputDirection2D = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+		Vector3 velo = Velocity;
 
-		var inputDirection3D = new Vector3(inputDirection2D.X, 0, inputDirection2D.Y);
+		//Quando estamos no chão a engine fisica impede que o valor vertical(Y) da velocidade diminua infinitamente
+		velo.Y -= gravity * (float)delta;//GRAVIDADE
 
-		var direction = Transform.Basis * inputDirection3D;
+		//MOVIMENTAÇÃO
+		Vector2 inputDirection2D = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+		Vector3 inputDirection3D = new Vector3(inputDirection2D.X, 0, inputDirection2D.Y);
+		Vector3 direction = Transform.Basis * inputDirection3D;
 		velo.X = direction.X * Speed;
 		velo.Z = direction.Z * Speed;
-		Velocity = velo;
 
+		//PULO
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+			velo.Y = jumpForce;
+		else if (Input.IsActionJustReleased("jump") && velo.Y > 0.0f)
+			velo.Y = 0.0f;
+
+
+		//APLICANDO OS MOVIMENTOS
+		Velocity = velo;
 		MoveAndSlide();
 	}
 
